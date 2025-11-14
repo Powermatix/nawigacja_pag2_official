ec613e900d2753003055fa88eb5628f70a284c9f
rafDanielak <rafdan8@gmail.com>
2025-11-14 13:07:41 +0100

    Graph creation finally added

---
+++ makeGraph.py
@@ -0,0 +1,45 @@
+import shapely
+from pyogrio.errors import DataSourceError
+from graph import Graph
+import geopandas as gpd
+
+roadClasses = {'droga wewnętrzna': 20, 'droga dojazdowa': 30, 'droga lokalna': 30, 'droga zbiorcza': 50,
+               'droga główna': 70, 'droga główna ruchu przyśpieszonego': 90, 'droga ekspresowa': 120, 'autostrada': 140}
+
+columnNames = {'klasa_drogi', 'klasa_drog', 'klasa'}
+
+
+def makeGraph(shapePath: str):
+    try:
+        lines = gpd.read_file(shapePath)
+    except DataSourceError:
+        print("Błąd odczytu pliku")
+        return None
+
+    graph = Graph()
+
+    lines.columns = lines.columns.str.lower()
+
+    if 'geometry' not in lines.columns:
+        print('Brak geometrii')
+        return None
+
+    colNameIntersection = columnNames.intersection(list(lines.columns))
+    if len(colNameIntersection) == 0:
+        print('Brak informacji o klasie drogi')
+
+        for line in lines.itertuples():
+            lineLength = shapely.length(line.geometry)
+            graph.add_edge(tuple(round(val, 0) for val in line.geometry.coords[0]),
+                           tuple(round(val, 0) for val in line.geometry.coords[-1]),
+                           lineLength)
+
+    else:
+        colName = colNameIntersection.pop()
+        for line in lines.itertuples():
+            lineLength = shapely.length(line.geometry)
+            graph.add_edge(tuple(round(val, 0) for val in line.geometry.coords[0]),
+                           tuple(round(val, 0) for val in line.geometry.coords[-1]),
+                           lineLength / roadClasses[getattr(line, colName)])
+
+    return graph
